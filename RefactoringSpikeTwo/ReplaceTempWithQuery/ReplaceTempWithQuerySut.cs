e746c3eab934a88ab0f7dcd44ed8f2551165fd9f
Matthew Butt <matthew@matthewbutt.com>
2015-05-27 11:17:41 +0100

    Tidy

---
+++ RefactoringSpikeTwo/ReplaceTempWithQuery/ReplaceTempWithQuerySut.cs
@@ -0,0 +1,23 @@
+﻿namespace RefactoringSpikeTwo.ReplaceTempWithQuery
+{
+    public class ReplaceTempWithQuerySut
+    {
+        private readonly int _itemPrice;
+        private readonly int _quantity;
+
+        public ReplaceTempWithQuerySut(int quantity, int itemPrice)
+        {
+            _quantity = quantity;
+            _itemPrice = itemPrice;
+        }
+
+        public double GetPrice()
+        {
+            var basePrice = _quantity*_itemPrice;
+            double discountFactor;
+            if (basePrice > 1000) discountFactor = 0.95;
+            else discountFactor = 0.98;
+            return basePrice*discountFactor;
+        }
+    }
+}
--- RefactoringSpikeTwo/ReplaceTempWithQuery/ReplaceTempWithQueryTests.cs
+++ RefactoringSpikeTwo/ReplaceTempWithQuery/ReplaceTempWithQueryTests.cs
@@ -25,25 +25,4 @@ namespace RefactoringSpikeTwo.ReplaceTempWithQuery
             Assert.That(price, Is.EqualTo(1900.0));
         }
     }
-
-    public class ReplaceTempWithQuerySut
-    {
-        private readonly int _itemPrice;
-        private readonly int _quantity;
-
-        public ReplaceTempWithQuerySut(int quantity, int itemPrice)
-        {
-            _quantity = quantity;
-            _itemPrice = itemPrice;
-        }
-
-        public double GetPrice()
-        {
-            int basePrice = _quantity*_itemPrice;
-            double discountFactor;
-            if (basePrice > 1000) discountFactor = 0.95;
-            else discountFactor = 0.98;
-            return basePrice*discountFactor;
-        }
-    }
 }
