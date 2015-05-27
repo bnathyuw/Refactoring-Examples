df35be5700b8d0651d551aaa88c7c5f6b51d05ee
Matthew Butt <matthew@matthewbutt.com>
2015-05-27 17:52:07 +0100

    another set of tests to be sure to be sure

--- RefactoringSpikeTwo/ReplaceTypeCodeWithClass/ReplaceTypeCodeWithClassTests.cs
+++ RefactoringSpikeTwo/ReplaceTypeCodeWithClass/ReplaceTypeCodeWithClassTests.cs
@@ -15,5 +15,19 @@ namespace RefactoringSpikeTwo.ReplaceTypeCodeWithClass
 
             Assert.That(person.BloodGroup, Is.EqualTo(bloodGroup));
         }
+
+        [TestCase(Person.O)]
+        [TestCase(Person.A)]
+        [TestCase(Person.B)]
+        [TestCase(Person.AB)]
+        public void A_persons_blood_group_can_be_amended(int bloodGroup)
+        {
+            var person = new Person(Person.O);
+            Assert.That(person.BloodGroup, Is.EqualTo(Person.O));
+
+            person.BloodGroup = bloodGroup;
+
+            Assert.That(person.BloodGroup, Is.EqualTo(bloodGroup));
+        }
     }
 }
