1e50af40d28c3b4994679dd2cc9a1cde9c8f8093
Matthew Butt <matthew@matthewbutt.com>
2015-05-27 11:16:16 +0100

    Setup for Replace Temp with Query

---
+++ RefactoringSpikeTwo/ReplaceTempWithQuery/ReplaceTempWithQueryTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace RefactoringSpikeTwo.ReplaceTempWithQuery
+{
+    [TestFixture]
+    public class ReplaceTempWithQueryTests
+    {
+        [Test]
+        public void Calculates_price_for_low_value_order()
+        {
+            var sut = new ReplaceTempWithQuerySut(20, 10);
+
+            var price = sut.GetPrice();
+
+            Assert.That(price, Is.EqualTo(196.0));
+        }
+
+        [Test]
+        public void Calculates_price_for_high_value_order()
+        {
+            var sut = new ReplaceTempWithQuerySut(20, 100);
+
+            var price = sut.GetPrice();
+
+            Assert.That(price, Is.EqualTo(1900.0));
+        }
+    }
+
+    public class ReplaceTempWithQuerySut
+    {
+        private readonly int _itemPrice;
+        private readonly int _quantity;
+
+        public ReplaceTempWithQuerySut(int quantity, int itemPrice)
+        {
+            _quantity = quantity;
+            _itemPrice = itemPrice;
+        }
+
+        public double GetPrice()
+        {
+            int basePrice = _quantity*_itemPrice;
+            double discountFactor;
+            if (basePrice > 1000) discountFactor = 0.95;
+            else discountFactor = 0.98;
+            return basePrice*discountFactor;
+        }
+    }
+}
