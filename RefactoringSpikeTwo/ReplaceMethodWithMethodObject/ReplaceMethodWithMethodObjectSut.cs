23ae434ace19689fc9f2e630862ce254bf08bd44
Matthew Butt <matthew@matthewbutt.com>
2015-05-27 12:21:27 +0100

    Replace method with method object

---
+++ RefactoringSpikeTwo/ReplaceMethodWithMethodObject/ReplaceMethodWithMethodObjectSut.cs
@@ -0,0 +1,15 @@
+﻿namespace RefactoringSpikeTwo.ReplaceMethodWithMethodObject
+{
+    public class ReplaceMethodWithMethodObjectSut
+    {
+        public int Gamma(int inputVal, int quantity, int yearToDate)
+        {
+            var importantValue1 = (inputVal*quantity);
+            var importantValue2 = (inputVal*yearToDate) + 100;
+            if ((yearToDate - importantValue2) > 100)
+                importantValue2 -= 20;
+            var importantValue3 = importantValue2*7;
+            return importantValue3 - 2*importantValue1;
+        }
+    }
+}
---
+++ RefactoringSpikeTwo/ReplaceMethodWithMethodObject/ReplaceMethodWithMethodObjectTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace RefactoringSpikeTwo.ReplaceMethodWithMethodObject
+{
+    [TestFixture]
+    public class ReplaceMethodWithMethodObjectTests
+    {
+        [Test]
+        public void Foo()
+        {
+            var sut = new ReplaceMethodWithMethodObjectSut();
+
+            var actual = sut.Gamma(2, 4, 6);
+
+            const int assured = 768;
+            Assert.That(actual, Is.EqualTo(assured));
+        }
+    }
+}
