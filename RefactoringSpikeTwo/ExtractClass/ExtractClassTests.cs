d168bf79c8e60670f139ba64fd9d2d95c1a59a88
Matthew Butt <matthew@matthewbutt.com>
2015-05-27 15:53:39 +0100

    Extract Class

---
+++ RefactoringSpikeTwo/ExtractClass/ExtractClassTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace RefactoringSpikeTwo.ExtractClass
+{
+    [TestFixture]
+    public class ExtractClassTests
+    {
+        [Test]
+        public void Has_correct_telephone_number()
+        {
+            var bloggs = new Person("Bloggs", "020", "1234 5678");
+
+            Assert.That(bloggs.TelephoneNumber, Is.EqualTo("(020) 1234 5678"));
+
+        }
+    }
+}
---
+++ RefactoringSpikeTwo/ExtractClass/Person.cs
@@ -0,0 +1,31 @@
+﻿namespace RefactoringSpikeTwo.ExtractClass
+{
+    public class Person
+    {
+        private readonly string _name;
+        private string _officeAreaCode;
+        private string _officeNumber;
+
+        public Person(string name, string officeAreaCode, string officeNumber)
+        {
+            _name = name;
+            OfficeAreaCode = officeAreaCode;
+            OfficeNumber = officeNumber;
+        }
+
+        public string Name { get { return _name; } }
+        public string TelephoneNumber { get { return "(" + _officeAreaCode + ") " + _officeNumber; } }
+
+        private string OfficeAreaCode
+        {
+            get { return _officeAreaCode; }
+            set { _officeAreaCode = value; }
+        }
+
+        string OfficeNumber
+        {
+            get { return _officeNumber; }
+            set { _officeNumber = value; }
+        }
+    }
+}
