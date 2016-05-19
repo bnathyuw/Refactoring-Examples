27d17b3416d8915deae5c827b7b0ae9ac3114cf8
Matthew Butt <matthew@codurance.com>
2016-05-19 17:13:45 +0100

    Added example of Inline Method

---
+++ RefactoringExamples/InlineMethod/Courier.cs
@@ -0,0 +1,22 @@
+﻿namespace RefactoringExamples.InlineMethod
+{
+    public class Courier
+    {
+        private readonly int _numberOfLateDeliveries;
+
+        public Courier(int numberOfLateDeliveries)
+        {
+            _numberOfLateDeliveries = numberOfLateDeliveries;
+        }
+
+        public int GetRating()
+        {
+            return (MoreThanFiveLateDeliveries()) ? 2 : 1;
+        }
+
+        private bool MoreThanFiveLateDeliveries()
+        {
+            return _numberOfLateDeliveries > 5;
+        }
+    }
+}
---
+++ RefactoringExamples/InlineMethod/CourierShould.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace RefactoringExamples.InlineMethod
+{
+    [TestFixture]
+    public class CourierShould
+    {
+        private const int Unreliable = 2;
+        private const int Reliable = 1;
+
+        [TestCase(6)]
+        [TestCase(10)]
+        [TestCase(100)]
+        public void Be_rated_unreliable_when_they_have_many_late_deliveries(int numberOfLateDeliveries)
+        {
+            Assert.That(new Courier(numberOfLateDeliveries).GetRating(), Is.EqualTo(Unreliable));
+        }
+
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(5)]
+        public void Be_rated_unreliable_when_they_have_many_few_deliveries(int numberOfLateDeliveries)
+        {
+            Assert.That(new Courier(numberOfLateDeliveries).GetRating(), Is.EqualTo(Reliable));
+        }
+    }
+}
