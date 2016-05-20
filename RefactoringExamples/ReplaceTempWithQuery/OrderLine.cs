de36c92d6ebd46b5db9f4bf57fcaabfd4e1eac3f
Matthew Butt <matthew@codurance.com>
2016-05-20 16:32:03 +0100

    Oops, I'm getting confused by Fowler's many similar examples.
    This reverts commit a64de8c24f4f62ca3b08708395c0c536ffa04f36.

--- RefactoringExamples/ReplaceTempWithQuery/OrderLine.cs
+++ RefactoringExamples/ReplaceTempWithQuery/OrderLine.cs
@@ -14,10 +14,12 @@
         public double GetPrice()
         {
             var basePrice = _quantity*_itemPrice;
+            double discountFactor;
             if (basePrice > 1000)
-                return basePrice * 0.95;
+                discountFactor = 0.95;
             else
-                return basePrice * 0.98;
+                discountFactor = 0.98;
+            return basePrice*discountFactor;
         }
     }
 }
