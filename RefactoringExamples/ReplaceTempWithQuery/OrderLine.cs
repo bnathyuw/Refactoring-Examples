a64de8c24f4f62ca3b08708395c0c536ffa04f36
Matthew Butt <matthew@codurance.com>
2016-05-20 16:30:16 +0100

    Make OrderLine implementation match the code in the book

--- RefactoringExamples/ReplaceTempWithQuery/OrderLine.cs
+++ RefactoringExamples/ReplaceTempWithQuery/OrderLine.cs
@@ -14,12 +14,10 @@
         public double GetPrice()
         {
             var basePrice = _quantity*_itemPrice;
-            double discountFactor;
             if (basePrice > 1000)
-                discountFactor = 0.95;
+                return basePrice * 0.95;
             else
-                discountFactor = 0.98;
-            return basePrice*discountFactor;
+                return basePrice * 0.98;
         }
     }
 }
