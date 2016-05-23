35816eae49ca7bf6f95e3720d68c12e9e4462c71
Matthew Butt <matthew@codurance.com>
2016-05-23 12:13:18 +0100

    IntroduceExplainingVariable example

---
+++ RefactoringExamples/IntroduceExplainingVariable/UserInterface.cs
@@ -0,0 +1,43 @@
+﻿namespace RefactoringExamples.IntroduceExplainingVariable
+{
+    public class UserInterface
+    {
+        private readonly ICollaborator _collaborator;
+        private bool _initialised;
+
+        public UserInterface(ICollaborator collaborator)
+        {
+            _collaborator = collaborator;
+            _initialised = false;
+        }
+
+        public void Initialise()
+        {
+            _initialised = true;
+        }
+
+        public void React(string platform, string browser, int resize)
+        {
+            if ((platform.ToUpper().IndexOf("MAC") > -1) && (browser.ToUpper().IndexOf("IE") > -1) && WasInitialised() &&
+                resize > 0)
+            {
+                DoSomething();
+            }
+        }
+
+        private bool WasInitialised()
+        {
+            return _initialised;
+        }
+
+        private void DoSomething()
+        {
+            _collaborator.DoSomething();
+        }
+    }
+
+    public interface ICollaborator
+    {
+        void DoSomething();
+    }
+}
---
+++ RefactoringExamples/IntroduceExplainingVariable/UserInterfaceShould.cs
@@ -0,0 +1,77 @@
+﻿using System.Threading;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace RefactoringExamples.IntroduceExplainingVariable
+{
+    [TestFixture]
+    public class UserInterfaceShould
+    {
+        private const string MacPlatform = "MAC";
+        private const string IeBrowser = "IE";
+        private const int SlightlyEnlarged = 1;
+        private ICollaborator _collaborator;
+        private UserInterface _userInterface;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _collaborator = Substitute.For<ICollaborator>();
+            _userInterface = new UserInterface(_collaborator);
+        }
+
+        [Test]
+        public void Do_something_when_using_initialised_ie_on_mac_and_enlarged(
+            [Values("I am on a mac computer", "I am on a MAC computer", "mac", MacPlatform, "Mac")] string platform,
+            [Values("I am using ie to browse", "I am using IE to browse", "ie", IeBrowser, "iE")] string browser,
+            [Values(SlightlyEnlarged, 2, 5, 100)] int resize)
+        {
+            _userInterface.Initialise();
+
+            _userInterface.React(platform, browser, resize);
+
+            _collaborator.Received().DoSomething();
+        }
+
+        [Test]
+        public void Not_do_something_when_not_initialised()
+        {
+            _userInterface.React(MacPlatform, IeBrowser, SlightlyEnlarged);
+
+            _collaborator.DidNotReceive().DoSomething();
+        }
+
+        [Test]
+        public void Not_do_something_when_not_on_mac()
+        {
+            _userInterface.Initialise();
+
+            _userInterface.React("WINDOWS", IeBrowser, SlightlyEnlarged);
+
+            _collaborator.DidNotReceive().DoSomething();
+        }
+
+        [Test]
+        public void Not_do_something_when_not_on_ie()
+        {
+            _userInterface.Initialise();
+
+            _userInterface.React(MacPlatform, "chrome", SlightlyEnlarged);
+
+            _collaborator.DidNotReceive().DoSomething();
+        }
+
+
+        [Test]
+        public void Not_do_something_when_resized_smaller(
+            [Values(0, -1, -2, -5, -100)] int resize)
+        {
+            _userInterface.Initialise();
+
+            _userInterface.React(MacPlatform, "chrome", resize);
+
+            _collaborator.DidNotReceive().DoSomething();
+        }
+
+    }
+}
