3a4a645394b6dc0e44769e72c51ec9b6a1c46855
Matthew Butt <matthew@codurance.com>
2016-06-20 17:47:39 +0100

    Micro-improvement to ExtractClass tests.

---
+++ RefactoringExamples/ExtractClass/PersonShould.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace RefactoringExamples.ExtractClass
+{
+    [TestFixture]
+    public class PersonShould
+    {
+        [Test]
+        public void Format_telephone_number()
+        {
+            const string officeAreaCode = "020";
+            const string officeNumber = "1234 5678";
+            var bloggs = new Person("Bloggs", officeAreaCode, officeNumber);
+
+            Assert.That(bloggs.TelephoneNumber, Is.EqualTo($"({officeAreaCode}) {officeNumber}"));
+
+        }
+    }
+}
